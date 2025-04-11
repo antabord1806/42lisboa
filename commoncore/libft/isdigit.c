ca36aa69dfbc1c7e4913addc7282f69248360ce3
Andre Filipe Taborda Guerreiro <antabord@c1r2s11.42lisboa.com>
2025-04-11 15:41:57 +0100

    secondcommit

--- commoncore/libft/isdigit.c
+++ commoncore/libft/isdigit.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   isalpha.c                                          :+:      :+:    :+:   */
+/*   isdigit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: antabord <antabord@student.42.fr>          #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-04-11 13:36:17 by antabord          #+#    #+#             */
-/*   Updated: 2025-04-11 13:36:17 by antabord         ###   ########.fr       */
+/*   Created: 2025-04-11 14:31:05 by antabord          #+#    #+#             */
+/*   Updated: 2025-04-11 14:31:05 by antabord         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include<stdio.h>
+#include <stdio.h>
 
-int ft_isalpha(int c)
+int ft_isdigit(int c)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    if (c >= '0' && c <= '9')
         return (1);
     return (0);
 }
 
 int main(void)
 {
-    printf("%d", ft_isalpha(68));
+    printf("%d", ft_isdigit(50));
     return (0);
 }
