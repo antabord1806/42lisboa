2adb2b93f54e8db7a1f1b9250e165f142c81b917
antabord <antabord@student.42lisboa.com>
2025-06-27 16:49:57 +0100

    ola

---
+++ commoncore/Push_Swap/push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antabord <antabord@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-06-27 13:57:25 by antabord          #+#    #+#             */
+/*   Updated: 2025-06-27 13:57:25 by antabord         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+char	push_swap(int ac, char *av);
+int		parsing(char *av);
+void	ft_err(void);
+
+#endif
